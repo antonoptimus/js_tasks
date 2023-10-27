a72587931befebce0f4df2be0ef5725d23c4886c
Anton Eliseev <anton_saintp@mail.ru>
2023-10-27 23:34:37 +0300

    best_time_to_buy/sell

---
+++ best_time.js
@@ -0,0 +1,17 @@
+let prices1 = [7, 1, 5, 3, 6, 4];
+let prices2 = [7, 6, 4, 3, 1];
+
+let maxProfit = (prices) => {
+  let result = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < prices[i + 1]) {
+      result += prices[i + 1] - prices[i];
+    }
+  }
+
+  return result;
+};
+
+console.log(maxProfit(prices1));
+console.log(maxProfit(prices2));
